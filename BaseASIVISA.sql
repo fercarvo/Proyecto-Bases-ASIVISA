e27a9d7270cd1440fe6350aac4726dd87350a5f6
Edgar Fernando Carvajal <efcu93@gmail.com>
2015-01-05 22:40:36 -0500

    Se crearon nuevas tablas en la BD

--- BaseASIVISA.sql
+++ BaseASIVISA.sql
@@ -1,1 +1,52 @@
 create database BaseASIVISA;
+use BaseASIVISA;
+
+create table Ejercicio(
+idEjercicio int,
+nombre varchar (20),
+descripcion varchar (100),
+intensidad varchar (8),
+calorias int,
+primary key (idEjercicio)
+);
+
+create table EjercicioRutina(
+ejercicio int,
+rutina int
+);
+
+create table Rutina(
+idRutina int,
+fechaInicio date,
+descripcion varchar (50),
+primary key (idRutina)
+);
+
+create table RutinaUsuario(
+rutina int,
+usuario int
+);
+
+create table EjercicioArea(
+ejercicio int,
+area varchar (8)
+);
+
+create table AreaDelCuerpo(
+idADC int,
+nombre varchar (20),
+primary key (idADC)
+);
+
+create table Usuario(
+idUsuario int,
+nombre varchar (39),
+email varchar (30),
+residencia varchar (30),
+pesoInicial float,
+estatura float,
+perfilPublico boolean,
+dificultadFísica varchar (30),
+primary key (idUsuario)
+);
+
