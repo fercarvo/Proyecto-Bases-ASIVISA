fa8665bd11b57ca194c42bbcebc283e96b810234
Edgar Fernando Carvajal <efcu93@gmail.com>
2015-01-07 13:34:43 -0500

    Se creo el conector a la BD

--- ConectorBDD.java
+++ ConectorBDD.java
@@ -1,12 +1,43 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author Fernando
- */
+
+import java.beans.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+
 public class ConectorBDD {
+    String url; 
+    String dbName;
+    String driver;
+    String userName; 
+    String password;
+    Connection con;
+    Statement st;
+
+    public ConectorBDD() {
+        url = "localhost"; 
+        dbName = "BaseASIVISA";
+        driver = "com.mysql.jdbc.Driver";
+        userName = "root"; 
+        password = "1991";
+        con = null;
+        st = null;
+    }
+    
+   public void conectar() throws SQLException{
+       
+       con = DriverManager.getConnection(url, userName, password);
+       st = (Statement) con.createStatement();
+   }
+   
+   public void desconectar() throws SQLException{
+       con.close();
+   }
+    
+    
+
+
+    
     
 }
