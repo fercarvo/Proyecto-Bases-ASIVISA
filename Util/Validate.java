c96b0db253471a7fd02ede71264414f434786f2f
scando1993 <scando@fiec.espol.edu.ec>
2015-01-07 08:40:12 -0500

    Clase para validar los datos
    Aqui se presentan metodos staticos los cuales pueden ser usados para
    validar el tamano, que sean solo letras o solo numeros

---
+++ Util/Validate.java
@@ -0,0 +1,86 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Util;
+
+/**
+ *
+ * @author Clases
+ */
+public class Validate {
+    public static boolean isOnlyAlfa(String name){
+        char[] chars = name.toCharArray();
+
+        for (char c : chars) {
+            if(!Character.isLetter(c)) {
+                return false;
+            }
+        }
+        return true;        
+    }
+
+    public static boolean isOnlyAlfa(String name, int validLength){
+        char[] chars = name.toCharArray();
+
+        if(chars.length != validLength)
+            return false;
+        
+        for (char c : chars) {
+            if(!Character.isLetter(c)) {
+                return false;
+            }
+        }
+        return true;        
+    }   
+    
+    public static boolean isOnlyNum(String name){
+        char[] chars = name.toCharArray();
+
+        for (char c : chars) {
+            if(!Character.isDigit(c)) {
+                return false;
+            }
+        }
+        return true;        
+    }
+    
+    public static boolean isOnlyNum(String name, int validLength){
+        char[] chars = name.toCharArray();
+
+        if(chars.length != validLength)
+            return false;
+        
+        for (char c : chars) {
+            if(!Character.isDigit(c)) {
+                return false;
+            }
+        }
+        return true;        
+    }
+    
+    public static boolean isOnlyAlfaNum(String name){
+        char[] chars = name.toCharArray();
+
+        for (char c : chars) {
+            if(!Character.isLetterOrDigit(c)) {
+                return false;
+            }
+        }
+        return true;        
+    }
+    
+    public static boolean isOnlyAlfaNum(String name, int validLength){
+        char[] chars = name.toCharArray();
+
+        if(chars.length != validLength)
+            return false;
+        
+        for (char c : chars) {
+            if(!Character.isLetterOrDigit(c)) {
+                return false;
+            }
+        }
+        return true;        
+    }
+}
