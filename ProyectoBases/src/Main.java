3d03eceba374dc63660a8da99d22037f74dd1300
scando1993 <scando@fiec.espol.edu.ec>
2015-01-07 08:47:47 -0500

    Se agrega el proyecto en netbeans
    Aqui esta el proyecto en el cual todos tenemos que trabajar por favor
    aqui mismo poner las diferentes funcionalidades para las diferentes
    ventanas.
    
    Tambien mantengamos el siguiente formato:
    - Paquetes individuales para cada uno de las ventanas, es decir los
    formularios que hacen la ventana usuario y otro para los formularios que
    hacen la ventana administrador
    - Clases especificas para diferentes funcionalidades en paquetes
    diferentes
    - La clase main se mantiene en el paquete default con el fin de evitar
    confundirla con alguna otra clase
    - El paquete util contiene todas las clases genericas que sirvan con el
    fin de agregar funcionalidad, es decir validacion, personalizacion, etc.

---
+++ ProyectoBases/src/Main.java
@@ -0,0 +1,24 @@
+
+import InterfacesUsuario.VtnBienvenido;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Clases
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        VtnBienvenido vtn = new VtnBienvenido();
+        vtn.setVisible(true);
+        
+    }
+}
