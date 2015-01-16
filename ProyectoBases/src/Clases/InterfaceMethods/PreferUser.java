a74b4109df06aa2669d972ae3e8eaa6864a7d365
scando1993 <scando@fiec.espol.edu.ec>
2015-01-16 15:19:41 -0500

    Esta clase permite el ingreso de preferencias por parte del usuario

---
+++ ProyectoBases/src/Clases/InterfaceMethods/PreferUser.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases.InterfaceMethods;
+
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Clases
+ */
+public class PreferUser extends JPanel{
+     public PreferUser(){
+         
+     }
+     private void initComponents(){
+         
+     }
+}
