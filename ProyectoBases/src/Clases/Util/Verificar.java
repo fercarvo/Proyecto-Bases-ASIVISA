9226a66332a15e3c30eaf712966a4a4a1a63f411
scando1993 <scando@fiec.espol.edu.ec>
2015-01-16 23:50:28 -0500

    Esta interfaz tiene que ser implementada por todos las clases que tengan validacion de datos

---
+++ ProyectoBases/src/Clases/Util/Verificar.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases.Util;
+
+/**
+ *
+ * @author Clases
+ */
+public interface Verificar {
+    public boolean verificarDatos();
+    public boolean datosVacios();
+    
+}
