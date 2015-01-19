199a94a3d3625e1f69a1f1ed4ac06d1150f9039d
scando1993 <scando@fiec.espol.edu.ec>
2015-01-19 02:00:52 -0500

    Se cambiaron de paquete la base de datos y la clase Main

---
+++ ProyectoBases/src/Tester/Main.java
@@ -0,0 +1,32 @@
+package Tester;
+
+
+import Clases.Util.ConectorBDD;
+import InterfacesUsuario.*;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Clases
+ */
+//
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ConectorBDD conector = new ConectorBDD();
+        VtnBienvenido vtn = new VtnBienvenido();
+        vtn.setVisible(false);
+        vtnAgregarComida vtn2 = new vtnAgregarComida();
+        vtn2.setVisible(false);
+        vtnCrearUsuario user = new vtnCrearUsuario(vtn2);
+        user.setVisible(true);
+    }
+}
