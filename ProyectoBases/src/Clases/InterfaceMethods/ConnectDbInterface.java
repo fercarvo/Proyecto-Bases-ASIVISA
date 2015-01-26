156e28e0dd4a8fb6f9684562b0ed82c654a5b531
scando1993 <scando@fiec.espol.edu.ec>
2015-01-26 03:32:53 -0500

    Interfaz que debe ser implementada por cada formulario para que cuando se cierre la ventana interna se manden los datos a la base de datos

---
+++ ProyectoBases/src/Clases/InterfaceMethods/ConnectDbInterface.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases.InterfaceMethods;
+
+/**
+ *
+ * @author Kevin
+ */
+public interface ConnectDbInterface {
+    
+    public boolean escribirDatosDB();
+}
