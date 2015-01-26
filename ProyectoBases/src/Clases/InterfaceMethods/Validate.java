f4b9cc81dbcd4ba0f35bdf2e758eeedf97af0470
scando1993 <scando@fiec.espol.edu.ec>
2015-01-26 03:39:28 -0500

    Solo se movieron las clases de los paquetes

---
+++ ProyectoBases/src/Clases/InterfaceMethods/Validate.java
@@ -0,0 +1,111 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases.InterfaceMethods;
+
+import Imagenes.imageResource;
+import Imagenes.imageSize;
+import java.awt.Dimension;
+import java.net.URL;
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author Clases
+ */
+public class Validate {
+    public static boolean isOnlyAlfa(String name){
+        if(name.isEmpty())
+            return false;
+                    
+        char[] chars = name.toCharArray();
+
+        for (char c : chars) {
+            if(!Character.isLetter(c)) {
+                return false;
+            }
+        }
+        return true;        
+    }
+
+    public static boolean isOnlyAlfa(String name, int validLength){
+        if(name.isEmpty())
+            return false;
+        
+        char[] chars = name.toCharArray();
+
+        if(chars.length != validLength)
+            return false;
+        
+        for (char c : chars) {
+            if(!Character.isLetter(c)) {
+                return false;
+            }
+        }
+        return true;        
+    }   
+    
+    public static boolean isOnlyNum(String name){
+        char[] chars = name.toCharArray();
+
+        if(name.isEmpty())
+            return false;
+        
+        for (char c : chars) {
+            if(!Character.isDigit(c)) {
+                return false;
+            }
+        }
+        return true;        
+    }
+    
+    public static boolean isOnlyNum(String name, int validLength){
+        char[] chars = name.toCharArray();
+
+        if(name.isEmpty())
+            return false;
+        
+        if(chars.length != validLength)
+            return false;
+        
+        for (char c : chars) {
+            if(!Character.isDigit(c)) {
+                return false;
+            }
+        }
+        return true;        
+    }
+    
+    public static boolean isOnlyAlfaNum(String name){
+        char[] chars = name.toCharArray();
+
+        if(name.isEmpty())
+            return false;
+        
+        for (char c : chars) {
+            if(!Character.isLetterOrDigit(c)) {
+                return false;
+            }
+        }
+        return true;        
+    }
+    
+    public static boolean isOnlyAlfaNum(String name, int validLength){
+        char[] chars = name.toCharArray();
+
+        if(name.isEmpty())
+            return false;
+        
+        if(chars.length != validLength)
+            return false;
+        
+        for (char c : chars) {
+            if(!Character.isLetterOrDigit(c)) {
+                return false;
+            }
+        }
+        return true;        
+    }
+    
+}
---
+++ ProyectoBases/src/Clases/InterfaceMethods/Verificar.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases.InterfaceMethods;
+
+/**
+ *
+ * @author Clases
+ */
+public interface Verificar {
+    public boolean verificarDatos();
+    public boolean datosVacios();
+}
