435a080814a7a63f6ef465a4d72d162c4f223f62
scando1993 <scando@fiec.espol.edu.ec>
2015-01-26 03:36:13 -0500

    Listener para los internal frames, esta clase permite que cuando se cierre una ventana interna se guarde la info inscrita en la base de datos

---
+++ ProyectoBases/src/Clases/InterfaceMethods/InternalFrameList.java
@@ -0,0 +1,58 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases.InterfaceMethods;
+
+import javax.swing.event.InternalFrameEvent;
+import javax.swing.event.InternalFrameListener;
+
+/**
+ *
+ * @author Kevin
+ */
+public class InternalFrameList implements InternalFrameListener{
+
+    private ConnectDbInterface container;
+    
+    public InternalFrameList(ConnectDbInterface cont) {
+        container = cont;
+    }
+
+    @Override
+    public void internalFrameOpened(InternalFrameEvent e) {
+        
+    }
+
+    @Override
+    public void internalFrameClosing(InternalFrameEvent e) {
+        boolean escribirDatosDB;
+        escribirDatosDB = container.escribirDatosDB();
+    }
+
+    @Override
+    public void internalFrameClosed(InternalFrameEvent e) {
+        
+    }
+
+    @Override
+    public void internalFrameIconified(InternalFrameEvent e) {
+        
+    }
+
+    @Override
+    public void internalFrameDeiconified(InternalFrameEvent e) {
+        
+    }
+
+    @Override
+    public void internalFrameActivated(InternalFrameEvent e) {
+        
+    }
+
+    @Override
+    public void internalFrameDeactivated(InternalFrameEvent e) {
+        
+    }
+    
+}
