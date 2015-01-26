01b244bb55f92a66fc8ba7f341825fb1ebb26f75
scando1993 <scando@fiec.espol.edu.ec>
2015-01-26 03:38:54 -0500

    Cada clase que contenga un panel debe tener una clase llamada [NOMBRE_CLASE]Callback la cual contiene todos los metodos de la misma por ejemplo: 	-El TDA ejercicio es llamado para crear, buscar o mostrar, entonces la clase EjerciciosCallback implementa los metodos que realizan 	 la creacion, la busqueda entre otros

---
+++ ProyectoBases/src/Clases/InterfaceMethods/Admin/EjerciciosCallback.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases.InterfaceMethods.Admin;
+
+import Clases.InterfaceMethods.InternalFrameList;
+import InterfacesAdministrador.vtnNueva;
+import javax.swing.JDesktopPane;
+
+/**
+ *
+ * @author Kevin
+ */
+public class EjerciciosCallback {
+    
+    private static int frameCountNuevo = 0;
+    private static int frameCountBusqueda = 0;
+    private static int frameCountEjercicio = 0;
+    
+    private static final int xOffset = 30, yOffset = 30;
+    
+    public void mnuBtnNuevEjerActionPerformed(java.awt.event.ActionEvent evt, JDesktopPane ventana) {                                               
+        // TODO add your handling code here:
+        EjercicioPanel panel = new EjercicioPanel();
+        InternalFrameList listener = new InternalFrameList(panel);
+        vtnNueva vtn = new vtnNueva(panel, "Ejercicio Nuevo " + (++frameCountNuevo));
+        vtn.setSize(300, 300);
+        vtn.setLocation(xOffset*frameCountNuevo, yOffset*frameCountNuevo);
+        vtn.setVisible(true);
+        ventana.add(vtn);
+        try{
+            vtn.setSelected(true);
+        }catch (java.beans.PropertyVetoException e){}
+        vtn.addInternalFrameListener(listener);
+        
+    }
+    
+    public void mnuBtnBusEjerciciosActionPerformed(java.awt.event.ActionEvent evt, JDesktopPane ventana) {                                                    
+        // TODO add your handling code here:
+        
+    }
+    
+    public void mnuBtnMostrarEjerActionPerformed(java.awt.event.ActionEvent evt, JDesktopPane ventana) {                                                  
+        // TODO add your handling code here:
+    }
+    
+}
